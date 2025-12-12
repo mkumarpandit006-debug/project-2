9c3bf736d9fbfbca65324e54def4d8587084a159
mkumarpandit006-debug <mkumarpandit006@gmail.com>
2025-12-12 15:04:58 +0530

    Update manu.js

--- js/manu.js
+++ js/manu.js
@@ -1,1 +1,25 @@
-
+ fetch("header.html")
+      .then(res => res.text())
+      .then(data => {
+        document.getElementById("header").innerHTML = data;
+        let currentPage = window.location.pathname.split("/").pop();
+        if ( !currentPage || currentPage == "#" ) {
+          currentPage = "index.html";
+        }
+        // menu me anchor tak pahuchne ka tareeka
+        let links = document.querySelectorAll(".menu .menubar ul li a");
+        // hr anchor tag ko process karenge
+        links.forEach(link => {
+          console.log("Current page:", currentPage);
+          //pehle  anchor tag ka href ki value save karenge
+          let linkPage = link.getAttribute("href");
+          console.log("Attribute Value: " , linkPage);
+          
+          if(!linkPage) return;
+  
+          if (linkPage == currentPage) {
+            link.closest("li").classList.add("underline_current");
+          } 
+        });
+      
+      });
