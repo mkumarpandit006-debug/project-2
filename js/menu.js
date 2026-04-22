246feaf8c2c464bdb4609c7c96ba408fd2866b0f
mkumarpandit006-debug <mkumarpandit006@gmail.com>
2026-04-22 23:15:48 +0530

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,25 +1,23 @@
- fetch("header.html")
-      .then(res => res.text())
-      .then(data => {
-        document.getElementById("header").innerHTML = data;
-        let currentPage = window.location.pathname.split("/").pop();
-        if ( !currentPage || currentPage == "#" ) {
-          currentPage = "index.html";
-        }
-        // menu me anchor tak pahuchne ka tareeka
-        let links = document.querySelectorAll(".menu .menubar ul li a");
-        // hr anchor tag ko process karenge
-        links.forEach(link => {
-          console.log("Current page:", currentPage);
-          //pehle  anchor tag ka href ki value save karenge
-          let linkPage = link.getAttribute("href");
-          console.log("Attribute Value: " , linkPage);
-          
-          if(!linkPage) return;
-  
-          if (linkPage == currentPage) {
-            link.closest("li").classList.add("underline_current");
-          } 
-        });
-      
-      });
+function addBook() {
+  let input = document.getElementById("bookInput");
+  let bookName = input.value;
+
+  if (bookName === "") {
+    alert("Enter book name");
+    return;
+  }
+
+  let li = document.createElement("li");
+  li.textContent = bookName;
+
+  let btn = document.createElement("button");
+  btn.textContent = "Remove";
+  btn.onclick = function () {
+    li.remove();
+  };
+
+  li.appendChild(btn);
+  document.getElementById("bookList").appendChild(li);
+
+  input.value = "";
+}
